a3b8016d8b05ec4be8bd55045db693b183b50e78
JarheadHME <JarheadHME@Gmail.com>
2024-08-24 05:23:11 -0400

    Add project files.

---
+++ EnemyHitboxView/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "UNT0026:GetComponent always allocates / Use TryGetComponent", Justification = "TryGetComponent is broken in GTFO Il2Cpp Environment")]
+[assembly: SuppressMessage("Performance", "UNT0022:Inefficient position/rotation assignment", Justification = "Preferred Clarity", Scope = "member", Target = "~M:EnemyHitboxView.EnemyHitboxes.Update")]
